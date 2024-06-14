7e2df82c409a47b35ab1e29cdea93f2a824c6d96
tlee08 <timothy.e.lee@sydney.edu.au>
2024-06-14 18:31:03 +1000

    abcd

--- behavysis_viewer/__main__.py
+++ behavysis_viewer/__main__.py
@@ -11,11 +11,10 @@ def main():
     window = MainWindow()
     window.show()
 
-    window.open(
-        # r"Z:\PRJ-BowenLab\TimLee\resources\neda_project\0_configs\608DVR_CH1_5_9_65_20240403112644.json"
-        r"z:\PRJ-BowenLab\TimLee\resources\project_ma\0_configs\2_Round1.1_20220530_AGG-MOA_test3-M3_a2.json"
-    )
-
+    # window.open(
+    #     # r"Z:\PRJ-BowenLab\TimLee\resources\neda_project\0_configs\608DVR_CH1_5_9_65_20240403112644.json"
+    #     r"z:\PRJ-BowenLab\TimLee\resources\project_ma\0_configs\2_Round1.1_20220530_AGG-MOA_test3-M3_a2.json"
+    # )
     # window.export_vid("example.mp4")
 
     sys.exit(app.exec())
