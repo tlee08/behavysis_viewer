52d272f3bb3b372b56ba9e61cfa100ded021c162
tlee08 <timothyeelim.lee@gmail.com>
2024-11-21 23:42:32 +0100

    abcd

--- behavysis_viewer/__main__.py
+++ behavysis_viewer/__main__.py
@@ -11,10 +11,10 @@ def main():
     window = MainWindow()
     window.show()
 
-    window.open(
-        # r"z:\PRJ-BowenLab\TimLee\resources\project_ma\0_configs\2_Round1.1_20220530_AGG-MOA_test3-M3_a2.json",
-        "/run/user/1000/gvfs/smb-share:server=shared.sydney.edu.au,share=research-data/PRJ-BowenLab/TimLee/resources/project_ma/0_configs/2_Round1.1_20220530_AGG-MOA_test3-M3_a2.json",
-    )
+    # window.open(
+    #     # r"z:\PRJ-BowenLab\TimLee\resources\project_ma\0_configs\2_Round1.1_20220530_AGG-MOA_test3-M3_a2.json",
+    #     "/run/user/1000/gvfs/smb-share:server=shared.sydney.edu.au,share=research-data/PRJ-BowenLab/TimLee/resources/project_ma/0_configs/2_Round1.1_20220530_AGG-MOA_test3-M3_a2.json",
+    # )
     # window.export_vid("example.mp4")
 
     sys.exit(app.exec())
